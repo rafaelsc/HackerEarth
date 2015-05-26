6582e5620ee5a4623b01f25f4c2b796095126ebf
Rafael Silveira Cordeiro <rafaelsc@rafaelsc.net>
2015-05-26 16:28:20 -0300

    Add Solution
    https://www.hackerearth.com/problem/algorithm/girlfriends-demands/

---
+++ csharp/Practice Programming Problems/Girlfriends demands.linq
@@ -0,0 +1,33 @@
+<Query Kind="Program">
+  <Namespace>System</Namespace>
+  <Namespace>System.Numerics</Namespace>
+</Query>
+
+public static class GirlfriendsDemandsProgram
+{
+    public static void Main(string[] args)
+    {
+        var demand = Console.ReadLine().Trim();
+        var numOfTests = long.Parse(Console.ReadLine().Trim());
+
+        for (var i = 0; i < numOfTests; i++)
+        {
+            var rndChoidsValues = (Console.ReadLine().Trim()).Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var n1 = long.Parse(rndChoidsValues[0]);
+            var n2 = long.Parse(rndChoidsValues[1]);
+
+            var result = CheckGirlfriendsDemands(demand, n1, n2);
+            Console.WriteLine(result);
+        }
+    }
+
+    public static string CheckGirlfriendsDemands(string demand, long n1, long n2)
+    {
+        var size = (long) demand.Length;
+        n1--; n2--;
+        var idx1 = (int) (n1 % size);
+        var idx2 = (int) (n2 % size);
+        var itIsAYes = (demand[idx1]) == (demand[idx2]);
+        return itIsAYes ? "Yes" : "No";
+    }
+}
---
+++ csharp/Practice Programming Problems/src/hackerearth/Girlfriends demands/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestProgram
+{
+    [Test]
+    public void Sample01()
+    {
+        var result1 = GirlfriendsDemandsProgram.CheckGirlfriendsDemands("vgxgp", 2, 4);
+        Assert.AreEqual("Yes", result1);
+    }
+    [Test]
+    public void Sample02()
+    {
+        var result1 = GirlfriendsDemandsProgram.CheckGirlfriendsDemands("vgxgp", 2, 5);
+        Assert.AreEqual("No", result1);
+    }
+    [Test]
+    public void Sample03()
+    {
+        var result1 = GirlfriendsDemandsProgram.CheckGirlfriendsDemands("vgxgp", 7, 14);
+        Assert.AreEqual("Yes", result1);
+    }
+
+    [Test]
+    public void Input01()
+    {
+        var result1 = GirlfriendsDemandsProgram.CheckGirlfriendsDemands("vgxgp", 252509054433933447, 760713016476190629);
+        Assert.AreEqual("Yes", result1);
+    }
+    [Test]
+    public void Input02()
+    {
+        var result1 = GirlfriendsDemandsProgram.CheckGirlfriendsDemands("vgxgp", 145645985530478284, 403591478376763604);
+        Assert.AreEqual("Yes", result1);
+    }
+
+
+}
